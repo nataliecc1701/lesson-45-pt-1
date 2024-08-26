0c25ef18a2a0e17292b23fbea492f6384863cfd8
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 14:43:48 -0700

    subsequence works

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,13 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    let [ptr1, ptr2] = [0,0];
+    
+    while (ptr1 < str1.length && ptr2 < str2.length) {
+        if (str1[ptr1] === str2[ptr2]) ptr1++;
+        else ptr2++;
+    }
+    
+    return ptr1 === str1.length
 }
+
+module.exports = isSubsequence
