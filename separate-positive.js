dba9321b4e9db0124c6c7fb0e9948fe297cb277f
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 14:36:45 -0700

    separate-positive works

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+    let left = 0;
+    let right = nums.length-1;
+    
+    while (left < right) {
+        if (nums[left] < 0 && nums[right] >= 0) {
+            [ nums[left], nums[right] ] = [ nums[right], nums[left] ];
+        }
+        else {
+            if (nums[left] >= 0) left++;
+            if (nums[right] < 0) right--;
+        }
+    }
+    
+    return nums;
 }
+
+module.exports = separatePositive
