c80cc9d9aece7610facd5c333e89f27698b75c15
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 14:03:19 -0700

    average-pair passes tests now

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(nums, target) {
+    let left = 0;
+    let right = nums.length-1;
+    
+    while (left < right) {
+        let sum = nums[left] + nums[right]
+        
+        if (sum === target*2) return true;
+        else if (sum < target*2) left++;
+        else right--;
+    }
+    return false;
 }
+
+module.exports = averagePair;
