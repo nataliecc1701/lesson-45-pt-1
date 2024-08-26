669679c4d1fc0b0ba3a0d0dc0edf3789a9f46b5c
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 14:08:11 -0700

    two-array-object done

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,11 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, vals) {
+    const retObj = {}
+    for (let i = 0; i < keys.length; i++) {
+        if (i >= vals.length) retObj[keys[i]] = null;
+        else retObj[keys[i]] = vals[i];
+    }
+    return retObj;
 }
+
+module.exports = twoArrayObject
