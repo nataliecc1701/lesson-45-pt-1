f137a8c1bfea2f645aa06d5753253f24a23612a2
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 13:57:33 -0700

    construct-note done

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,25 @@
+function freqCount(str) {
+    const counter = new Map();
+    
+    for (const char of str) {
+        let valCount = counter.get(char) || 0;
+        counter.set(char, valCount + 1);
+    }
+    
+    return counter;
+}
+
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, letters) {
+    if (msg.length > letters.length) return false;
+    
+    const msgFreq = freqCount (msg);
+    const lettersFreq = freqCount(letters);
+    
+    for (const key of msgFreq.keys()) {
+        if (!lettersFreq.has(key)) return false;
+        if (msgFreq.get(key) > lettersFreq.get(key)) return false;
+    }
+    
+    return true;
 }
