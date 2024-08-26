5f4feebe3d689b7f8ca164bebb28e3422d5d2932
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 14:15:57 -0700

    same-frequency works

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,18 @@
-// add whatever parameters you deem necessary
-function sameFrequency() {
+function digitFreqCount(num) {
+    const digits = [0,0,0,0,0,0,0,0,0,0]
+    
+    for (const digit of String(num)) {
+        digits[digit]++;
+    }
+    
+    return digits;
 }
+
+// add whatever parameters you deem necessary
+function sameFrequency(num1, num2) {
+    const freqs = [digitFreqCount(num1), digitFreqCount(num2)]
+    const comparables = freqs.map((freq) => freq.join())
+    return comparables[0] === comparables[1];
+}
+
+module.exports = sameFrequency;
