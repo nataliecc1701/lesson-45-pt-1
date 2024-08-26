b59565f15f92edb685fa9562ee08c3057b851d8f
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-08-26 14:56:14 -0700

    count-pairs works

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,13 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(nums, target) {
+    const numSet = new Set(nums);
+    let count = 0;
+    
+    for (const num of nums) {
+        if (target - num > num && numSet.has(target-num)) count++;
+    }
+    
+    return count;
 }
+
+module.exports = countPairs;
